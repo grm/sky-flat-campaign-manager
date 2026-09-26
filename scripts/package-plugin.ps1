1a8307a786f3683a50c6ea2fbac41a82f996efad
Jérémie Klein <grm.klein@gmail.com>
2026-09-26 18:08:48 +0200

    Default package version to 0.0.9

--- scripts/package-plugin.ps1
+++ scripts/package-plugin.ps1
@@ -1,7 +1,7 @@
 param(
   [string]$Configuration = "Release",
   [string]$OutputDir = "artifacts/package",
-  [string]$Version = "0.0.8"
+  [string]$Version = "0.0.9"
 )
 
 $ErrorActionPreference = "Stop"
